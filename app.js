aa7501b522a1072cc38f55675115c01e94f19f8f
jongho Kim <31331701+eodl123@users.noreply.github.com>
2023-11-21 21:00:24 +0900

    javacript-timer
    슈퍼코딩 자바스크립트 연습코드

---
+++ app.js
@@ -0,0 +1,12 @@
+const 시작_시간 = new Date();
+
+function setTime() {
+  const 현재_시간 = new Date();
+  const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+  const 분 = 흐른_시간.getMinutes().toString().padStart(2, "0");
+  const 초 = 흐른_시간.getSeconds().toString().padStart(2, "0");
+  const timeH1 = document.querySelector(".time");
+  timeH1.innerText = `${분}:${초}`;
+}
+
+setInterval(setTime, 1000);
